8c66921e208ea5b62254f80287bcd053f238c3c6
b-hkm <74144509+b-hkm@users.noreply.github.com>
2025-05-20 08:01:03 +0100

    fixes

--- Kamiyomi/manga_mngr.py
+++ Kamiyomi/manga_mngr.py
@@ -42,7 +42,7 @@ def add_url(command):
     if manga_page.status_code != 200:
         print(f"status error:{ manga_page.status_code }")
     manga_title=BeautifulSoup(manga_page,'html.parser')
-    print(manga_title.find(title))
+    print("Manga ", str(manga_title.find(title))," successfully add")
     #print("Manga ", manganelo.get_story_page(url = command).title, "successfully added")
     try:
         f.write(command + "\n")
